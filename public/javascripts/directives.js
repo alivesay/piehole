a396982b664d3b236c1109037730eb57ac777aa6
Andrew Livesay <andrew.livesay@gmail.com>
2013-02-11 02:18:53 -0500

    added font-awesome; extended rating directive

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -15,11 +15,13 @@ angular.module('pieholeApp.directives', [])
       compile: function(tElement, tAttrs) {
         var content = tElement.children();
         
-        for (var i = 0; i < tAttrs.count - 1; i++) {
+        for (var i = 1; i < tAttrs.count; i++) {
           var nContent = content.clone();
           nContent.addClass('n' + i);
           tElement.append(nContent);
         }
+        
+        content.addClass('n0');
         tElement.replaceWith(tElement.children());
       }
     };
--- public/javascripts/modules/rating/rating.js
+++ public/javascripts/modules/rating/rating.js
@@ -1,16 +1,23 @@
 'use strict';
 
+// should this have it's own controller?
+
 angular.module('pieholeApp.directives')
   .directive('uiRating', function () {
     return {
       restrict: 'E',
       replace: true,
+      
+      scope: { currentValue: '=currentValue',
+               maxValue:     '=maxValue' },
+               
       template: '<div class="ui-rating">' +
                 '  <data-ph-ntimes count="5"><span class="ui-rating-star">☆</span></data>' +
                 '</div>',
-      link: function postLink(scope, iElement, iAttrs, controller) {
-        console.log(iElement);
-        iElement.children().each(function() {
+                
+      link: function postLink(scope, element, attrs, controller) {
+        element.children().each(function(i) {
+          $(this).toggleClass('empty', (i > scope.currentValue));
         });
       }
     };
